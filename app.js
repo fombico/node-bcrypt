ae1b0fb3c2d47e99bed23fb573ed22324a4a6704
Fredrich Ombico <fombico@pivotal.io>
2017-09-08 11:15:41 -0400

    Add testing of hash

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ var bcrypt = require('bcrypt');
 bcrypt.genSalt(10, (err, salt) => {
 		if (err) return next(err);
 
+		console.log("Hashing passw0rd..");
 		bcrypt.hash("passw0rd", salt, (err, hash) => {
 			if (err) return next(err);
 
 			console.log('salt: %s', salt);
 			console.log('hash: %s', hash);
+
+			bcrypt.compare("password", hash, (err, match) => {
+				console.log("Testing password: " + match);
+			});
+
+
+			bcrypt.compare("passw0rd", hash, (err, match) => {
+				console.log("Testing passw0rd: " + match);
+			});
 		});
 	});
